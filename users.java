508c1b3db8ec57d9b23492680836d7c01ae211c4
saumya001 <saumya001@users.noreply.github.com>
2015-05-05 11:32:36 +0530

    Create users.java

---
+++ users.java
@@ -0,0 +1,12 @@
+package examportal.dto;
+public class Users {
+	private String username;
+	private String userid;
+	private String userpass;
+	public String getUsername() { return username; }
+	public void setUsername(String username) { this.username = username; }
+	public String getUserid() { return userid; }
+	public void setUserid(String userid) { this.userid = userid; }
+	public String getUserpass() { return userpass; }
+	public void setUserpass(String userpass) { this.userpass = userpass; }
+}
