91b8f1c1169bd5f3d295e074d4b9eeb2b4478610
saumya001 <saumya001@users.noreply.github.com>
2015-05-05 11:31:55 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,75 @@
+package examportal.dto;
+public class Test {
+	private String q1=null;
+	private String q1opa=null;
+	private String q1opb=null;
+	private String q1opc=null;
+	private String q1opd=null;
+	private String q2=null;
+	private String q2opa=null;
+	private String q2opb=null;
+	private String q2opc=null;
+	private String q2opd=null;
+	private String q3=null;
+	private String q3opa=null;
+	private String q3opb=null;
+	private String q3opc=null;
+	private String q3opd=null;
+	private String q4=null;
+	private String q5=null;
+	private String a1=null;
+	private String a2=null;
+	private String a3=null;
+	private String a4=null;
+	private String a5=null;
+
+	public String getQ1() { return q1; }
+	public void setQ1(String q1) { this.q1 = q1; }
+	public String getQ1opa() { return q1opa; }
+	public void setQ1opa(String q1opa) { this.q1opa = q1opa; }
+	public String getQ1opb() { return q1opb; }
+	public void setQ1opb(String q1opb) { this.q1opb = q1opb; }
+	public String getQ1opc() { return q1opc; }
+	public void setQ1opc(String q1opc) { this.q1opc = q1opc; }
+	public String getQ1opd() { return q1opd; }
+	public void setQ1opd(String q1opd) { this.q1opd = q1opd; }
+	public String getA1() { return a1; }
+	public void setA1(String a1) { this.a1 = a1; }
+
+	public String getQ2() { return q2; }
+	public void setQ2(String q2) { this.q2 = q2; }
+	public String getQ2opa() { return q2opa; }
+	public void setQ2opa(String q2opa) { this.q2opa = q2opa; }
+	public String getQ2opb() { return q2opb; }
+	public void setQ2opb(String q2opb) { this.q2opb = q2opb; }
+	public String getQ2opc() { return q2opc; }
+	public void setQ2opc(String q2opc) { this.q2opc = q2opc; }
+	public String getQ2opd() { return q2opd; }
+	public void setQ2opd(String q2opd) { this.q2opd = q2opd; }
+	public String getA2() { return a2; }
+	public void setA2(String a2) { this.a2 = a2; }
+
+	public String getQ3() { return q3; }
+	public void setQ3(String q3) { this.q3 = q3; }
+	public String getQ3opa() { return q3opa; }
+	public void setQ3opa(String q3opa) { this.q3opa = q3opa; }
+	public String getQ3opb() { return q3opb; }
+	public void setQ3opb(String q3opb) { this.q3opb = q3opb; }
+	public String getQ3opc() { return q3opc; }
+	public void setQ3opc(String q3opc) { this.q3opc = q3opc; }
+	public String getQ3opd() { return q3opd; }
+	public void setQ3opd(String q3opd) { this.q3opd = q3opd; }
+	public String getA3() { return a3; }
+	public void setA3(String a3) { this.a3 = a3; }
+
+	public String getQ4() { return q4; }
+	public void setQ4(String q4) { this.q4 = q4; }
+	public String getA4() { return a4; }
+	public void setA4(String a4) { this.a4 = a4; }
+
+	public String getQ5() { return q5; }
+	public void setQ5(String q5) { this.q5 = q5; }
+	public String getA5() { return a5; }
+	public void setA5(String a5) { this.a5 = a5; }
+}
+
