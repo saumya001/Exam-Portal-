18774168fd654503e7a18b371b560d512e67bce8
saumya001 <saumya001@users.noreply.github.com>
2015-05-05 11:26:39 +0530

    Create logoutservlet.java

---
+++ logoutservlet.java
@@ -0,0 +1,15 @@
+package examportal;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet({ "/logoutservlet", "/logout" })
+public class logoutservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getSession().setAttribute("stat", null);
+		response.sendRedirect("/Exam_Portal");
+	}
+}
